599c41652ae3cebfaea6d76639658ca80ad205f6
10331 <kiritowdnmd@gmail.com>
2023-07-02 13:38:21 +0800

    feat: profile page

---
+++ common/src/main/java/com/mrl/pixiv/common/ui/components/UserAvatar.kt
@@ -0,0 +1,43 @@
+package com.mrl.pixiv.common.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
+import coil.transform.Transformation
+
+@Composable
+fun UserAvatar(
+    url: String,
+    modifier: Modifier = Modifier,
+    size: Dp = 40.dp,
+    onClick: () -> Unit = {},
+    contentDescription: String? = null,
+    contentScale: ContentScale = ContentScale.Fit,
+    tint: Color = Color.Unspecified,
+    transformations: List<Transformation> = listOf(CircleCropTransformation()),
+) {
+    AsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(url).allowRgb565(true)
+            .transformations(transformations)
+            .build(),
+        contentDescription = contentDescription,
+        contentScale = contentScale,
+        modifier = modifier
+            .size(size)
+            .clickable(onClick = onClick)
+    )
+}
