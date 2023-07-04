51fa4d928ff569ac7f01ce3106947b9e7e96b43d
Darrious Liu <60057825+master-lzh@users.noreply.github.com>
2023-07-04 16:23:53 +0800

    feat(util): add ComposeUtil

--- common/src/main/java/com/mrl/pixiv/common/ui/components/UserAvatar.kt
+++ common/src/main/java/com/mrl/pixiv/common/ui/components/UserAvatar.kt
@@ -1,14 +1,9 @@
 package com.mrl.pixiv.common.ui.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
@@ -17,6 +12,7 @@ import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
 import coil.transform.Transformation
+import com.mrl.pixiv.util.click
 
 @Composable
 fun UserAvatar(
@@ -38,6 +34,6 @@ fun UserAvatar(
         contentScale = contentScale,
         modifier = modifier
             .size(size)
-            .clickable(onClick = onClick)
+            .click(onClick = onClick)
     )
 }
---
+++ util/src/main/java/com/mrl/pixiv/util/ComposeUtil.kt
@@ -0,0 +1,50 @@
+package com.mrl.pixiv.util
+
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.semantics.Role
+
+
+const val VIEW_CLICK_INTERVAL_TIME = 1000L//点击间隔时间
+
+inline fun Modifier.click(
+    time: Long = VIEW_CLICK_INTERVAL_TIME,
+    enabled: Boolean = true,//中间这三个是clickable自带的参数
+    onClickLabel: String? = null,
+    role: Role? = null,
+    crossinline onClick: () -> Unit
+): Modifier = composed {
+    composed {
+        var lastClickTime by remember { mutableStateOf(value = 0L) }//使用remember函数记录上次点击的时间
+        clickable(enabled, onClickLabel, role) {
+            val currentTimeMillis = System.currentTimeMillis()
+            if (currentTimeMillis - time >= lastClickTime) {//判断点击间隔,如果在间隔内则不回调
+                onClick()
+                lastClickTime = currentTimeMillis
+            }
+        }
+    }
+}
+
+@Composable
+inline fun composeClick(
+    time: Long = VIEW_CLICK_INTERVAL_TIME,
+    crossinline onClick: () -> Unit
+): () -> Unit {
+    //使用remember函数记录上次点击的时间
+    var lastClickTime by remember { mutableStateOf(value = 0L) }
+    return {
+        val currentTimeMillis = System.currentTimeMillis()
+        //判断点击间隔,如果在间隔内则不回调
+        if (currentTimeMillis - time >= lastClickTime) {
+            onClick()
+            lastClickTime = currentTimeMillis
+        }
+    }
+}
