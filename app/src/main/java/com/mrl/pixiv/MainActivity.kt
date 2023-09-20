850926f1e5ad4c9aed89dd0a752b30ee15628e02
Darrious Liu <60057825+master-lzh@users.noreply.github.com>
2023-09-20 22:33:55 +0800

    feat(Main): 每隔半小时刷新token

--- app/src/main/java/com/mrl/pixiv/MainActivity.kt
+++ app/src/main/java/com/mrl/pixiv/MainActivity.kt
@@ -5,22 +5,33 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import com.mrl.pixiv.common.base.BaseActivity
+import com.mrl.pixiv.data.auth.GrantType
 import com.mrl.pixiv.navigation.root.RootNavigationGraph
 import com.mrl.pixiv.splash.SplashUiIntent
 import com.mrl.pixiv.splash.SplashViewModel
 import com.mrl.pixiv.theme.PiPixivTheme
+import kotlinx.coroutines.delay
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.time.Duration.Companion.minutes
 
 class MainActivity : BaseActivity() {
     private val splashViewModel: SplashViewModel by viewModel()
+
     @Composable
     override fun BuildContent() {
+        LaunchedEffect(Unit) {
+            while (true) {
+                delay(30.minutes)
+                splashViewModel.dispatch(SplashUiIntent.RefreshAccessTokenIntent(GrantType.REFRESH_TOKEN))
+            }
+        }
         PiPixivTheme {
             val state by splashViewModel.uiStateFlow.collectAsStateWithLifecycle()
             Surface(
