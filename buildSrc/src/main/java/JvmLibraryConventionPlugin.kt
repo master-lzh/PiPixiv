1fe6fb62b11363668ee3f640d0b7c0aba62e9740
Darrious Liu <60057825+master-lzh@users.noreply.github.com>
2023-11-06 23:23:51 +0800

    build(deps): 使用kotlin 2.0

--- buildSrc/src/main/java/JvmLibraryConventionPlugin.kt
+++ buildSrc/src/main/java/JvmLibraryConventionPlugin.kt
@@ -17,12 +17,21 @@
 import com.mrl.pixiv.buildsrc.configureKotlinJvm
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 
 class JvmLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
                 apply("org.jetbrains.kotlin.jvm")
+            }
+            extensions.configure<KotlinAndroidProjectExtension> {
+                sourceSets.all {
+                    languageSettings {
+                        languageVersion = "2.0"
+                    }
+                }
             }
             configureKotlinJvm()
         }
