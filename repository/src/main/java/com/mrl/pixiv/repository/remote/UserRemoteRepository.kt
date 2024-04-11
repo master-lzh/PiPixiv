51994d0d1cee0ae96deb9caf2464113aac143371
Darrious Liu <60057825+master-lzh@users.noreply.github.com>
2024-04-11 19:47:33 +0800

    refactor(di): 切换依赖注入为更简洁的constructor dsl

--- repository/src/main/java/com/mrl/pixiv/repository/remote/UserRemoteRepository.kt
+++ repository/src/main/java/com/mrl/pixiv/repository/remote/UserRemoteRepository.kt
@@ -7,28 +7,25 @@ import com.mrl.pixiv.data.user.UserFollowAddReq
 import com.mrl.pixiv.data.user.UserFollowDeleteReq
 import com.mrl.pixiv.data.user.UserIllustsQuery
 import com.mrl.pixiv.datasource.remote.UserHttpService
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.flowOn
 
 class UserRemoteRepository(
     private val userHttpService: UserHttpService,
-    private val ioDispatcher: CoroutineDispatcher,
 ) {
     suspend fun getUserDetail(userDetailQuery: UserDetailQuery) =
-        userHttpService.getUserDetail(userDetailQuery).flowOn(ioDispatcher)
+        userHttpService.getUserDetail(userDetailQuery)
 
     suspend fun getUserIllusts(userIllustsQuery: UserIllustsQuery) =
-        userHttpService.getUserIllusts(userIllustsQuery).flowOn(ioDispatcher)
+        userHttpService.getUserIllusts(userIllustsQuery)
 
     suspend fun getUserBookmarksIllust(userBookmarksIllustQuery: UserBookmarksIllustQuery) =
-        userHttpService.getUserBookmarksIllust(userBookmarksIllustQuery).flowOn(ioDispatcher)
+        userHttpService.getUserBookmarksIllust(userBookmarksIllustQuery)
 
     suspend fun getUserBookmarksNovels(userBookmarksNovelQuery: UserBookmarksNovelQuery) =
-        userHttpService.getUserBookmarksNovels(userBookmarksNovelQuery).flowOn(ioDispatcher)
+        userHttpService.getUserBookmarksNovels(userBookmarksNovelQuery)
 
     suspend fun followUser(userFollowAddReq: UserFollowAddReq) =
-        userHttpService.followUser(userFollowAddReq).flowOn(ioDispatcher)
+        userHttpService.followUser(userFollowAddReq)
 
     suspend fun unFollowUser(userFollowDeleteReq: UserFollowDeleteReq) =
-        userHttpService.unFollowUser(userFollowDeleteReq).flowOn(ioDispatcher)
+        userHttpService.unFollowUser(userFollowDeleteReq)
 }
