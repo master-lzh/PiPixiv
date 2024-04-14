a014931f589d122019a8fda54159c8b4be52d459
Darrious Liu <60057825+master-lzh@users.noreply.github.com>
2024-04-14 19:14:46 +0800

    fix(setting): 下拉菜单位置问题

--- feature/setting/src/main/java/com/mrl/pixiv/setting/components/DropDownSelector.kt
+++ feature/setting/src/main/java/com/mrl/pixiv/setting/components/DropDownSelector.kt
@@ -2,10 +2,13 @@ package com.mrl.pixiv.setting.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowDropDown
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -17,16 +20,27 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun DropDownSelector(
     modifier: Modifier = Modifier,
+    expanded: Boolean,
+    onDismissRequest: () -> Unit,
     current: String,
+    items: @Composable ColumnScope.() -> Unit
 ) {
-    Row(
-        modifier = modifier
-            .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.5f), MaterialTheme.shapes.small)
-            .padding(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(text = current, style = MaterialTheme.typography.bodyMedium)
-        Icon(imageVector = Icons.Rounded.ArrowDropDown, contentDescription = null)
+    Box {
+        Row(
+            modifier = modifier
+                .background(
+                    MaterialTheme.colorScheme.primary.copy(alpha = 0.5f),
+                    MaterialTheme.shapes.small
+                )
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = current, style = MaterialTheme.typography.bodyMedium)
+            Icon(imageVector = Icons.Rounded.ArrowDropDown, contentDescription = null)
+        }
+        DropdownMenu(expanded = expanded, onDismissRequest = onDismissRequest) {
+            items()
+        }
     }
 }
