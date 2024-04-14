b07d76c7dcbc1084e9d0e871109989b32b4a02ec
Darrious Liu <60057825+master-lzh@users.noreply.github.com>
2024-04-14 20:27:12 +0800

    fix(setting): 修复Android 8以后主题才有follow system

--- repository/src/main/java/com/mrl/pixiv/repository/local/SettingLocalRepository.kt
+++ repository/src/main/java/com/mrl/pixiv/repository/local/SettingLocalRepository.kt
@@ -1,5 +1,6 @@
 package com.mrl.pixiv.repository.local
 
+import android.os.Build
 import com.mrl.pixiv.data.setting.SettingTheme
 import com.mrl.pixiv.datasource.local.SettingDataSource
 import kotlinx.coroutines.flow.map
@@ -7,7 +8,9 @@ import kotlinx.coroutines.flow.map
 class SettingLocalRepository(
     private val settingDataSource: SettingDataSource
 ) {
-    val settingTheme = settingDataSource.settingTheme.get(SettingTheme.SYSTEM.toString()).map {
+    val settingTheme = settingDataSource.settingTheme.get(
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) SettingTheme.SYSTEM.toString() else SettingTheme.LIGHT.toString()
+    ).map {
         enumValueOf<SettingTheme>(it)
     }
 
