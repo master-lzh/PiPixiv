c7c3d4b0fa5d9580853791a2e1447134cee02ebc
Darrious Liu <60057825+master-lzh@users.noreply.github.com>
2024-08-17 01:21:40 +0800

    feat(build): Set Kotlin language version to 2.0
    - 更新Kotlin语言版本到2.0
    - 适用于Kotlin和Compose编译设置

--- buildSrc/src/main/java/com/mrl/pixiv/buildsrc/KotlinAndroid.kt
+++ buildSrc/src/main/java/com/mrl/pixiv/buildsrc/KotlinAndroid.kt
@@ -27,6 +27,7 @@ import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 /**
@@ -85,6 +86,7 @@ private fun Project.configureKotlin() {
     // Use withType to workaround https://youtrack.jetbrains.com/issue/KT-55947
     tasks.withType<KotlinCompile>().configureEach {
         compilerOptions {
+            languageVersion.set(KotlinVersion.KOTLIN_2_0)
             // Set JVM target to 11
             jvmTarget.set(JvmTarget.JVM_11)
             // Treat all Kotlin warnings as errors (disabled by default)
