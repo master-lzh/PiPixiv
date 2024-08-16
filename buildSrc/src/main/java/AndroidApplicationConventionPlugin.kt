35e22ec006f77f49f3423a99d136a1824fcb6e1d
Darrious Liu <60057825+master-lzh@users.noreply.github.com>
2024-08-17 01:51:38 +0800

    fix(plugin): 添加Kotlin Compose插件支持
    - 增加了对Compose插件的应用
    - 优化了插件管理部分

--- buildSrc/src/main/java/AndroidApplicationConventionPlugin.kt
+++ buildSrc/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -27,6 +27,7 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
             with(pluginManager) {
                 apply("com.android.application")
                 apply("org.jetbrains.kotlin.android")
+                apply("org.jetbrains.kotlin.plugin.compose")
                 apply("com.google.devtools.ksp")
             }
 
