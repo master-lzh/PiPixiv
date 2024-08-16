28d02e6ecf9562b1a6b51b450c346cd791813e39
Darrious Liu <60057825+master-lzh@users.noreply.github.com>
2024-08-17 04:03:30 +0800

    opt(reducer): 优化简化Reducer逻辑
    - 采用扩展函数简化reduce函数定义
    - 减少状态复制的冗余代码
    - 提升代码可读性和维护性

--- common-middleware/src/main/java/com/mrl/pixiv/common/middleware/illust/IllustReducer.kt
+++ common-middleware/src/main/java/com/mrl/pixiv/common/middleware/illust/IllustReducer.kt
@@ -3,11 +3,11 @@ package com.mrl.pixiv.common.middleware.illust
 import com.mrl.pixiv.common.viewmodel.Reducer
 
 class IllustReducer : Reducer<IllustState, IllustAction> {
-    override fun reduce(state: IllustState, action: IllustAction): IllustState {
+    override fun IllustState.reduce(action: IllustAction): IllustState {
         return when (action) {
             is IllustAction.SetIllust -> {
-                state.illusts.put(action.illustId, action.illust)
-                state
+                illusts.put(action.illustId, action.illust)
+                this
             }
         }
     }
