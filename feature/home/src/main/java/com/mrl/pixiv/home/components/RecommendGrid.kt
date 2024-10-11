5bbcdcee374a828306f888a0a6e5143c6e4ef23f
Darrious Liu <60057825+master-lzh@users.noreply.github.com>
2024-10-11 17:22:46 +0800

    opt(home): 使用Paging库优化推荐图像列表
    - 引入Paging库替代原始分页加载逻辑
    - 移除手动管理的刷新和加载更多逻辑，改用Paging3处理
    - 调整HomeScreen和RecommendGrid相关组件以支持LazyPagingItems
    - 在Gradle配置中添加Paging依赖
    - 优化异常处理，统一通过collectAsLazyPagingItems收集数据

--- feature/home/src/main/java/com/mrl/pixiv/home/components/RecommendGrid.kt
+++ feature/home/src/main/java/com/mrl/pixiv/home/components/RecommendGrid.kt
@@ -7,28 +7,26 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridState
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.foundation.lazy.staggeredgrid.items
-import androidx.compose.foundation.lazy.staggeredgrid.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.itemKey
 import com.mrl.pixiv.common.viewmodel.bookmark.BookmarkState
 import com.mrl.pixiv.data.Illust
 import com.mrl.pixiv.util.DisplayUtil
-import com.mrl.pixiv.util.OnScrollToBottom
-import kotlinx.collections.immutable.ImmutableList
 
 private const val LOADING_ITEM_COUNT = 4
 private const val INCLUDE_EDGE = true
 
 @Composable
 fun RecommendGrid(
+    recommendImageList: LazyPagingItems<Illust>,
     navToPictureScreen: (Illust, String) -> Unit,
     bookmarkState: BookmarkState,
     lazyStaggeredGridState: LazyStaggeredGridState,
-    recommendImageList: ImmutableList<Illust>,
-    onScrollToBottom: () -> Unit,
 ) {
     val spanCount = when (LocalConfiguration.current.orientation) {
         ORIENTATION_PORTRAIT -> 2
@@ -46,32 +44,42 @@ fun RecommendGrid(
         verticalItemSpacing = 3.dp,
         modifier = Modifier.fillMaxSize()
     ) {
-        items(recommendImageList, key = { it.id }) {
-            val isBookmarked = bookmarkState.state[it.id] ?: it.isBookmarked
-            RecommendImageItem(
-                width = width,
-                navToPictureScreen = navToPictureScreen,
-                illust = it,
-                isBookmarked = isBookmarked,
-                onBookmarkClick = { restrict, tags ->
-                    if (isBookmarked) {
-                        bookmarkState.deleteBookmarkIllust(it.id)
-                    } else {
-                        bookmarkState.bookmarkIllust(it.id, restrict, tags)
+        items(recommendImageList.itemCount, key = recommendImageList.itemKey { it.id }) {
+            val illust = recommendImageList[it]
+            if (illust != null) {
+                val isBookmarked = bookmarkState.state[illust.id] ?: illust.isBookmarked
+                RecommendImageItem(
+                    width = width,
+                    navToPictureScreen = navToPictureScreen,
+                    illust = illust,
+                    isBookmarked = isBookmarked,
+                    onBookmarkClick = { restrict, tags ->
+                        if (isBookmarked) {
+                            bookmarkState.deleteBookmarkIllust(illust.id)
+                        } else {
+                            bookmarkState.bookmarkIllust(illust.id, restrict, tags)
+                        }
                     }
-                }
-            )
-        }
-
-        if (recommendImageList.isNotEmpty()) {
-            itemsIndexed(
-                List(LOADING_ITEM_COUNT) { 0 },
-                key = { index, _ -> "loading-$index" }) { _, _ ->
-                RecommendSkeleton(size = width)
+                )
             }
         }
-    }
-    lazyStaggeredGridState.OnScrollToBottom(loadingItemCount = LOADING_ITEM_COUNT) {
-        onScrollToBottom()
+
+        when (recommendImageList.loadState.append) {
+            is LoadState.Loading -> { // Pagination Loading UI
+                items(LOADING_ITEM_COUNT) {
+                    RecommendSkeleton(size = width)
+                }
+            }
+
+            else -> {}
+        }
+
+//        if (recommendImageList.isNotEmpty()) {
+//            itemsIndexed(
+//                List(LOADING_ITEM_COUNT) { 0 },
+//                key = { index, _ -> "loading-$index" }) { _, _ ->
+//                RecommendSkeleton(size = width)
+//            }
+//        }
     }
 }
