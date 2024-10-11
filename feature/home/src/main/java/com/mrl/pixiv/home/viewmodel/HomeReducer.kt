fae63f547a9afbfabb70521728d133772b840067
liuzhenhui <liuzhenhui@highlight.mobi>
2024-10-11 17:22:46 +0800

    opt(home): 使用Paging库优化推荐图像列表
    - 引入Paging库替代原始分页加载逻辑
    - 移除手动管理的刷新和加载更多逻辑，改用Paging3处理
    - 调整HomeScreen和RecommendGrid相关组件以支持LazyPagingItems
    - 在Gradle配置中添加Paging依赖
    - 优化异常处理，统一通过collectAsLazyPagingItems收集数据

--- feature/home/src/main/java/com/mrl/pixiv/home/viewmodel/HomeReducer.kt
+++ feature/home/src/main/java/com/mrl/pixiv/home/viewmodel/HomeReducer.kt
@@ -8,8 +8,6 @@ class HomeReducer : Reducer<HomeState, HomeAction> {
     override fun HomeState.reduce(action: HomeAction): HomeState {
         return when (action) {
             is HomeAction.UpdateState -> action.state
-            is HomeAction.DismissLoading -> copy(isRefresh = false)
-            is HomeAction.RefreshIllustRecommendedIntent -> copy(isRefresh = true)
             else -> this
         }
     }
