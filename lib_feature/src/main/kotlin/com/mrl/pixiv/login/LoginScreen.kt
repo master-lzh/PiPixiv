32377996c5347b9b204db2de6256b6f89c3616d1
Darrious <60057825+master-lzh@users.noreply.github.com>
2025-03-02 21:16:39 +0800

    refactor(ui): 更新LoginScreen组件以使用Material3的Scaffold和相关组件

--- lib_feature/src/main/kotlin/com/mrl/pixiv/login/LoginScreen.kt
+++ lib_feature/src/main/kotlin/com/mrl/pixiv/login/LoginScreen.kt
@@ -6,10 +6,22 @@ import android.util.Log
 import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
 import android.webkit.WebView
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
@@ -98,7 +110,6 @@ internal fun LoginScreen(
     }
     Scaffold(
         modifier = modifier
-            .statusBarsPadding()
             .imePadding(),
         topBar = {
             TopAppBar(
