7e5d5ec996b6eae819b4897a7fc04dbdb7be809b
Darrious <60057825+master-lzh@users.noreply.github.com>
2025-05-14 15:09:07 +0800

    feat(profile): 重构ProfileDetailScreen结构，优化代码逻辑和可读性
    - 分离AppBar逻辑至独立Composable `ProfileDetailAppBar`，提高代码复用性
    - 重构`ProfileDetailScreen`参数和内部逻辑，简化调用方式并移除冗余代码
    - 使用`navHostController`直接处理导航逻辑，减少额外传参
    - 增强背景图显示逻辑，维持UI一致性

--- lib_feature/src/main/kotlin/com/mrl/pixiv/profile/detail/ProfileDetailScreen.kt
+++ lib_feature/src/main/kotlin/com/mrl/pixiv/profile/detail/ProfileDetailScreen.kt
@@ -29,11 +29,15 @@ import androidx.navigation.NavHostController
 import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
 import coil3.request.allowRgb565
+import com.mrl.pixiv.common.data.user.UserDetailResp
 import com.mrl.pixiv.common.kts.spaceBy
 import com.mrl.pixiv.common.ui.LocalNavigator
 import com.mrl.pixiv.common.ui.components.UserAvatar
 import com.mrl.pixiv.common.ui.currentOrThrow
-import com.mrl.pixiv.common.util.*
+import com.mrl.pixiv.common.util.RString
+import com.mrl.pixiv.common.util.copyToClipboard
+import com.mrl.pixiv.common.util.navigateToPictureScreen
+import com.mrl.pixiv.common.util.throttleClick
 import com.mrl.pixiv.common.viewmodel.asState
 import com.mrl.pixiv.feature.R
 import com.mrl.pixiv.profile.detail.components.IllustWidget
@@ -42,20 +46,6 @@ import org.koin.androidx.compose.koinViewModel
 import org.koin.core.parameter.parametersOf
 import kotlin.math.pow
 
-@Composable
-fun ProfileDetailScreen(
-    uid: Long,
-    modifier: Modifier = Modifier,
-    navHostController: NavHostController = LocalNavigator.currentOrThrow,
-    profileDetailViewModel: ProfileDetailViewModel = koinViewModel { parametersOf(uid) },
-) {
-    ProfileDetailScreen(
-        modifier = modifier,
-        state = profileDetailViewModel.asState(),
-        navToPictureScreen = navHostController::navigateToPictureScreen,
-    ) { navHostController.popBackStack() }
-}
-
 private const val KEY_USER_INFO = "user_info"
 private const val KEY_USER_ILLUSTS = "user_illusts"
 private const val KEY_USER_BOOKMARKS_ILLUSTS = "user_bookmarks_illusts"
@@ -63,97 +53,26 @@ private const val KEY_USER_BOOKMARKS_NOVELS = "user_bookmarks_novels"
 private const val KEY_SPACE = "space"
 
 @Composable
-internal fun ProfileDetailScreen(
+fun ProfileDetailScreen(
+    uid: Long,
     modifier: Modifier = Modifier,
-    state: ProfileDetailState,
-    navToPictureScreen: NavigateToHorizontalPictureScreen = { _, _, _ -> },
-    popBack: () -> Unit = { },
+    navHostController: NavHostController = LocalNavigator.currentOrThrow,
+    viewModel: ProfileDetailViewModel = koinViewModel { parametersOf(uid) },
 ) {
+    val state = viewModel.asState()
     val userInfo = state.userInfo
-//    val backgroundHeight = when (LocalConfiguration.current.orientation) {
-//        Configuration.ORIENTATION_PORTRAIT -> DisplayUtil.getScreenWidthDp() / 3
-//        Configuration.ORIENTATION_LANDSCAPE -> DisplayUtil.getScreenHeightDp() / 3
-//        else -> DisplayUtil.getScreenWidthDp() / 3
-//    }
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     val lazyListState = rememberLazyListState()
-    val avatarSize = 50.dp
-    val expandedHeight = TopAppBarDefaults.MediumAppBarExpandedHeight + avatarSize
-    val backgroundHeight = TopAppBarDefaults.MediumAppBarExpandedHeight +
-            WindowInsets.statusBars.asPaddingValues().calculateTopPadding() +
-            avatarSize * scrollBehavior.state.collapsedFraction.pow(2) +
-            with(LocalDensity.current) {
-                scrollBehavior.state.heightOffset.toDp()
-            }
 
     Scaffold(
         modifier = modifier
             .fillMaxSize()
             .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            val backgroundUrl = userInfo.profile.backgroundImageURL.ifEmpty {
-                userInfo.user.profileImageUrls.medium
-            }
-            if (backgroundUrl.isNotEmpty()) {
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data(backgroundUrl)
-                        .allowRgb565(true)
-                        .build(),
-                    contentScale = ContentScale.FillWidth,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(backgroundHeight)
-                        .blur(10.dp)
-                        .drawWithCache {
-                            val color = Color.Black.copy(alpha = 0.5f)
-                            onDrawWithContent {
-                                drawContent()
-                                drawRect(color)
-                            }
-                        }
-                )
-            }
-            MediumTopAppBar(
-                title = {
-                    Row(
-                        verticalAlignment = CenterVertically
-                    ) {
-                        UserAvatar(
-                            url = userInfo.user.profileImageUrls.medium,
-                            modifier = Modifier.size(avatarSize * (2 - scrollBehavior.state.collapsedFraction)),
-                        )
-                        if (scrollBehavior.state.collapsedFraction == 1f) {
-                            Text(
-                                modifier = Modifier.padding(start = 10.dp),
-                                text = userInfo.user.name,
-                                style = TextStyle(
-                                    fontSize = 20.sp,
-                                    fontWeight = FontWeight.Medium,
-                                ),
-                            )
-                        }
-                    }
-                },
-                modifier = Modifier.statusBarsPadding(),
-                navigationIcon = {
-                    IconButton(
-                        onClick = popBack,
-                        modifier = Modifier.padding(vertical = 10.dp)
-                    ) {
-                        Icon(imageVector = Icons.Rounded.ArrowBackIosNew, contentDescription = null)
-                    }
-                },
-                expandedHeight = expandedHeight,
-                windowInsets = WindowInsets(0),
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = Color.Transparent,
-                    scrolledContainerColor = Color.Transparent,
-                    navigationIconContentColor = Color.White,
-                    titleContentColor = Color.White,
-                ),
-                scrollBehavior = scrollBehavior
+            ProfileDetailAppBar(
+                userInfo = userInfo,
+                scrollBehavior = scrollBehavior,
+                onBack = navHostController::popBackStack
             )
         },
     ) {
@@ -232,7 +151,7 @@ internal fun ProfileDetailScreen(
                             RString.illustration_count,
                             userInfo.profile.totalIllusts
                         ),
-                        navToPictureScreen = navToPictureScreen,
+                        navToPictureScreen = navHostController::navigateToPictureScreen,
                         illusts = state.userIllusts,
                         modifier = Modifier.fillMaxWidth()
                     )
@@ -245,7 +164,7 @@ internal fun ProfileDetailScreen(
                     IllustWidget(
                         title = stringResource(RString.illust_and_manga_liked),
                         endText = stringResource(RString.view_all),
-                        navToPictureScreen = navToPictureScreen,
+                        navToPictureScreen = navHostController::navigateToPictureScreen,
                         illusts = state.userBookmarksIllusts,
                         modifier = Modifier.fillMaxWidth()
                     )
@@ -271,4 +190,85 @@ internal fun ProfileDetailScreen(
             }
         }
     }
+}
+
+@Composable
+private fun ProfileDetailAppBar(
+    userInfo: UserDetailResp,
+    scrollBehavior: TopAppBarScrollBehavior,
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val avatarSize = 50.dp
+    val expandedHeight = TopAppBarDefaults.MediumAppBarExpandedHeight + avatarSize
+    val backgroundHeight = TopAppBarDefaults.MediumAppBarExpandedHeight +
+            WindowInsets.statusBars.asPaddingValues().calculateTopPadding() +
+            avatarSize * scrollBehavior.state.collapsedFraction.pow(2) +
+            with(LocalDensity.current) {
+                scrollBehavior.state.heightOffset.toDp()
+            }
+    val backgroundUrl = userInfo.profile.backgroundImageURL.ifEmpty {
+        userInfo.user.profileImageUrls.medium
+    }
+    if (backgroundUrl.isNotEmpty()) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(backgroundUrl)
+                .allowRgb565(true)
+                .build(),
+            contentScale = ContentScale.FillWidth,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(backgroundHeight)
+                .blur(10.dp)
+                .drawWithCache {
+                    val color = Color.Black.copy(alpha = 0.5f)
+                    onDrawWithContent {
+                        drawContent()
+                        drawRect(color)
+                    }
+                }
+        )
+    }
+    MediumTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = CenterVertically
+            ) {
+                UserAvatar(
+                    url = userInfo.user.profileImageUrls.medium,
+                    modifier = Modifier.size(avatarSize * (2 - scrollBehavior.state.collapsedFraction)),
+                )
+                if (scrollBehavior.state.collapsedFraction == 1f) {
+                    Text(
+                        modifier = Modifier.padding(start = 10.dp),
+                        text = userInfo.user.name,
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Medium,
+                        ),
+                    )
+                }
+            }
+        },
+        modifier = modifier.statusBarsPadding(),
+        navigationIcon = {
+            IconButton(
+                onClick = onBack,
+                modifier = Modifier.padding(vertical = 10.dp)
+            ) {
+                Icon(imageVector = Icons.Rounded.ArrowBackIosNew, contentDescription = null)
+            }
+        },
+        expandedHeight = expandedHeight,
+        windowInsets = WindowInsets(0),
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = Color.Transparent,
+            scrolledContainerColor = Color.Transparent,
+            navigationIconContentColor = Color.White,
+            titleContentColor = Color.White,
+        ),
+        scrollBehavior = scrollBehavior
+    )
 }
