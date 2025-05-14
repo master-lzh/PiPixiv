80952bab83093e699cf70b3ee91996f708df875d
Darrious <60057825+master-lzh@users.noreply.github.com>
2025-05-14 19:20:23 +0800

    feat(build): 升级Java版本至21及代码优化
    - 更新GitHub Actions中release.yml和develop.yml的Java版本至21
    - 合并Theme.kt中MaterialTheme相关的import语句以优化代码

--- app/src/main/kotlin/com/mrl/pixiv/theme/Theme.kt
+++ app/src/main/kotlin/com/mrl/pixiv/theme/Theme.kt
@@ -2,11 +2,7 @@ package com.mrl.pixiv.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
